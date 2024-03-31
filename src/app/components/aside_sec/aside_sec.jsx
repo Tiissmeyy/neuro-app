081562da6ed5a328127c48be201dfa58898683a3
Tiissmeyy <wilson_erwan1@yahoo.com>
2024-03-31 01:27:51 +0100

    add images subpages

--- src/app/components/aside_sec/aside_sec.jsx
+++ src/app/components/aside_sec/aside_sec.jsx
@@ -1,22 +1,33 @@
 import Image from "next/image";
 import styles from "./aside_sec.module.css"
 
-const Aside_Sec = ({open_nav}) => {
+const Aside_Sec = ({open_menu, selected_content}) => {
+    const images_array = [
+        "/img/bg_home_small.jpg",
+        "/img/psycho.jpg",
+        "/img/emdr.jpg",
+        "/img/neuro.jpg",
+        "/img/cogni.jpg"
+    ]
+    console.log(selected_content)
     return (
     
         <>
-            <aside className={`${styles.image_container} ${!open_nav ? "visible": "unvisible"}`}>
-            <Image src="/img/bg_home.jpg" fill></Image>
+            <aside className={`${styles.image_container} ${!open_menu ? "visible": "unvisible"}`}>
+            <Image src={images_array[selected_content]} alt="aside image" fill>
+            </Image>
+                
+            
             <div className={styles.footer}>
                 <div className={styles.footer_logo}>
-                    <Image src="/img/neuro_logo2.png" width={20} height={20}></Image>
+                    <Image src={"/img/neuro_logo2.png"} alt="logo" width={20} height={20}></Image>
                 </div>
                 <p className={styles.footer_name}>​​ -  Suelen Amorim  -</p>
                 <p className={styles.footer_adress}>Cabinet de Psychologie & Neuropsychologie  -  42, quai Charles Roissard 73000 Chambery</p>
                 <p className={styles.footer_number}>07 83 24 43 02 </p>
             </div>
             </aside>
-            <nav className={`${styles.image_container} ${styles.nav_container} ${!open_nav ? "unvisible": "visible"}`}>
+            <nav className={`${styles.image_container} ${styles.nav_container} ${!open_menu ? "unvisible": "visible"}`}>
                 <a>ACCUEIL</a>
                 <a>QUI-SUIS JE ?</a>
                 <a>LE CABINET</a>
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./page.module.css";
 import subpages from "./data/subpages.json"
 import Main_Page from "./components/main_page/main_page";
@@ -12,6 +12,28 @@ import Banner from "./components/banner/banner";
 export default function Home() {
     const [open_menu, set_open_menu] = useState(false)
     const [selected_content, set_selected_content] = useState(0)
+    const [doc_size, set_doc_size] = useState(1000)
+
+    const filter_ref = useRef(null)
+
+    useEffect(() => {
+        const resize = () => {
+            set_doc_size(document.documentElement.scrollHeight);
+        };
+
+        resize();
+        window.addEventListener('resize', resize);
+
+        return () => {
+            window.removeEventListener('resize', resize);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (filter_ref.current) {
+            filter_ref.current.style.height = `${doc_size}px`;
+        }
+    }, [doc_size]);
 
     const selection_array = [
         <Main_Page key={0} />,
@@ -33,11 +55,11 @@ export default function Home() {
 
   return (
     <>
-        <Aside_Sec open_nav={open_menu} />
+        <Aside_Sec selected_content={selected_content} open_menu={open_menu} />
         <main className={styles.main}>
             {selection_array[selected_content]}
             <Banner handle_selection={handle_selection} />
-            <div className={`${styles.filter} ${open_menu ? "visible": "unvisible"}`}>
+            <div ref={filter_ref} className={`${styles.filter} ${open_menu ? "visible": "unvisible"}`}>
             </div> 
         </main>
         <div className={styles.menu_div} onClick={()=>handle_click()}>
